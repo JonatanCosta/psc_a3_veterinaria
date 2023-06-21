8916c0cab455b5b0548c3ad0556eff529cdf3f83
Jonatan Costa <jonatan.costa@appmax.com.br>
2023-06-21 20:14:41 -0300

    Ajustado gitignore para ignorar a pasta .ideia e a classe de conexão

--- src/DAO/ConexaoExample.java
+++ src/DAO/ConexaoExample.java
@@ -3,14 +3,15 @@ package DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-public class Conexao {
+
+public class ConexaoExample {
     public Connection criarConexao() {
         String usuario = "root";
-        String senha = "lari2305";
-        String base = "clinica_vet";
+        String senha = "root";
+        String base = "sistema_pet";
 
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + base, usuario, senha);
+            return DriverManager.getConnection("jdbc:mysql://localhost:3310/" + base, usuario, senha);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
