5df72ac4191e53cfa9c8c196876c05319a6b7678
Jonatan Costa <jonatan.costa@appmax.com.br>
2023-06-22 19:42:43 -0300

    Criado AgendamentoDAO

---
+++ src/DAO/AgendamentoDAO.java
@@ -0,0 +1,19 @@
+package DAO;
+
+public class AgendamentoDAO {
+    public void cadastrarAgendamento() {
+
+    }
+
+    public void listarAgendamentos() {
+
+    }
+
+    public void excluirAgendamento() {
+
+    }
+
+    public void editarAgendamento() {
+
+    }
+}
