7fa05a1035dd9a35bcc88c34e47d4457ed562e63
Jonatan Costa <jonatan.costa@appmax.com.br>
2023-06-22 19:40:09 -0300

    Criado classe de servico de agendamento

---
+++ src/Servico/AgendamentoServico.java
@@ -0,0 +1,20 @@
+package Servico;
+
+public class AgendamentoServico {
+
+    public void cadastrarAgendamento() {
+
+    }
+
+    public void listarAgendamentos() {
+
+    }
+
+    public void excluirAgendamento() {
+
+    }
+
+    public void editarAgendamento() {
+
+    }
+}
